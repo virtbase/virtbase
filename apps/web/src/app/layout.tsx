afd48df1df2c8454cb671a56a25549f6a1c0e588
Janic <contact@janic.dev>
2026-03-16 14:12:24 +0100

    feat: init shadcn ui

---
+++ apps/web/src/app/layout.tsx
@@ -0,0 +1,36 @@
+/*
+ *   Copyright (c) 2026 Janic Bellmann
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import type { Viewport } from "next";
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  userScalable: true,
+  colorScheme: "dark light",
+  themeColor: [
+    { media: "(prefers-color-scheme: dark)", color: "#0a0a0a" },
+    { media: "(prefers-color-scheme: light)", color: "#ffffff" },
+  ],
+};
+
+// TODO: Add base metadata
+// export const metadata = constructMetadata();
+
+export default function RootLayout({ children }: LayoutProps<"/">) {
+  return children;
+}
---
+++ apps/web/src/ui/document.tsx
@@ -0,0 +1,51 @@
+/*
+ *   Copyright (c) 2026 Janic Bellmann
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import "@/styles/globals.css";
+
+import { cn } from "@virtbase/ui";
+
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geist = Geist({
+  subsets: ["latin"],
+  variable: "--font-geist-sans",
+});
+
+const geistMono = Geist_Mono({
+  subsets: ["latin"],
+  variable: "--font-geist-mono",
+});
+
+export default function Document({
+  children,
+  locale,
+}: Readonly<{ children: React.ReactNode; locale: string }>) {
+  return (
+    <html lang={locale} className="dark" suppressHydrationWarning>
+      <body
+        className={cn(
+          "bg-background font-sans text-foreground antialiased",
+          geist.variable,
+          geistMono.variable,
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
