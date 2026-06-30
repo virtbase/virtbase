135c014efc7777328b09fef424ed005da491e5c3
Janic <contact@janic.dev>
2026-06-30 17:58:52 +0200

    chore(deps): bump

--- packages/api/src/stripe/client.ts
+++ packages/api/src/stripe/client.ts
@@ -19,7 +19,7 @@ import Stripe from "stripe";
 
 export const stripe = process.env.STRIPE_SECRET_KEY
   ? new Stripe(process.env.STRIPE_SECRET_KEY, {
-      apiVersion: "2026-05-27.dahlia",
+      apiVersion: "2026-06-24.dahlia",
       typescript: true,
       telemetry: false,
     })
